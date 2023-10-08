782cc788568f9e2fdc5fcadb19c3c4b3e7ea8ea5
Виктория Абраменко <abramenko.victoria@gmail.com>
2023-10-08 15:33:45 +0300

    дз

--- homework.pas
+++ homework.pas
@@ -1,1 +1,14 @@
-﻿aaa
+﻿begin
+println('На каком языке вы разговариваете?');
+println('Введите: "1" - если на русском');
+println('"2" - если на английском');
+println('"3" - если на немецком');
+println('"4" - если на французском');
+var a := ReadInteger;
+if a = 1 then print('Привет!')
+else if a = 2 then print('Hello!')
+else if a = 3 then print('Hallo!')
+else if a = 4 then print('Bonjour!')
+else print('нет языка с таким номером(');
+end.
+
